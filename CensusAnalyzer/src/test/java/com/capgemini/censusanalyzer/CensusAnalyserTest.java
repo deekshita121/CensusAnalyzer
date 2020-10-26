c4fb5fb0f7b1e8e9af8a4caaa7aa0971e6ccb036
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-26 09:56:27 +0530

    [deekshita] Refactor : Refactored the code to throw custom exception when enetered invalid type

--- CensusAnalyzer/src/test/java/com/capgemini/censusanalyzer/CensusAnalyserTest.java
+++ CensusAnalyzer/src/test/java/com/capgemini/censusanalyzer/CensusAnalyserTest.java
@@ -6,11 +6,14 @@ import org.junit.Test;
 
 import org.junit.Assert;
 import org.junit.Test;
+
 import com.capgemini.censusanalyzer.CensusAnalyserException.ExceptionType;
 
 public class CensusAnalyserTest {
-	private static final String INDIA_CENSUS_CSV_FILE = "C:\\Users\\Lenovo\\git\\CensusAnalyser\\CensusAnalyser\\src\\test\\resources\\IndianStateCensusData-IndianStateCensusData.csv";
-	private static final String INCORRECT_INDIA_CENSUS_CSV_FILE = "C:\\Users\\Lenovo\\git\\CensusAnalyser\\CensusAnalyser\\src\\test\\resources\\_IndianStateCensusData-IndianStateCensusData.csv";
+	private static final String INDIA_CENSUS_CSV_FILE = "C:\\Users\\Lenovo\\git\\CensusAnalyzer\\CensusAnalyzer\\src\\test\\resources\\IndianStateCensusData-IndianStateCensusData.csv";
+	private static final String INCORRECT_INDIA_CENSUS_CSV_FILE = "C:\\Users\\Lenovo\\git\\CensusAnalyzer\\CensusAnalyzer\\src\\test\\resources\\_IndianStateCensusData-IndianStateCensusData.csv";
+	private static final String WRONG_CSV_FILE_TYPE = "C:\\Users\\Lenovo\\git\\CensusAnalyzer\\CensusAnalyzer\\src\\test\\resources\\_IndianStateCensusData-IndianStateCensusData.txt";
+
 	@Test
 	public void givenTheIndiaCensusCSVFile_WhenRead_NoOfRecordsShouldMatch() throws CensusAnalyserException {
 		CensusAnalyser censusAnalyser = new CensusAnalyser();
@@ -26,5 +29,13 @@ public class CensusAnalyserTest {
 		censusAnalyser.loadIndiaCensusData(INCORRECT_INDIA_CENSUS_CSV_FILE);
 		Assert.assertEquals(ExceptionType.CENSUS_FILE_PROBLEM.toString(), exceptionMessage);
 
+	}
+	
+	@Test
+	public void givenIncorrectType_ShouldReturnCustomException() throws CensusAnalyserException {
+		String exceptionMessage = null;
+		CensusAnalyser censusAnalyser = new CensusAnalyser();
+		censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_TYPE);
+		Assert.assertEquals(ExceptionType.CENSUS_FILE_PROBLEM.toString(), exceptionMessage);
 	}
 }
