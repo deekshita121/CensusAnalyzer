328274bd07ecb4e0bd3d78091df9294b5a82da25
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-26 22:20:21 +0530

    [deekshita] Add : Ability to report the data into a json file and sort based on density

--- CensusAnalyzer/src/test/java/com/capgemini/censusanalyzer/CensusAnalyserTest.java
+++ CensusAnalyzer/src/test/java/com/capgemini/censusanalyzer/CensusAnalyserTest.java
@@ -129,6 +129,15 @@ public class CensusAnalyserTest {
 		censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE, CSVBuilderType.OPEN_CSV);
 		String sortedCensusData = censusAnalyser.getStateWiseSortedCensusData();
 		CSVIndiaCensus[] censusList = new Gson().fromJson(sortedCensusData, CSVIndiaCensus[].class);
-		Assert.assertEquals(censusList[0].state, "Andhra Pradesh");
+		Assert.assertEquals(censusList[0].state, "Uttar Pradesh");
+	}
+	
+	@Test
+	public void givenIndianCensus_WhenSortedShouldReturn_Density() throws CensusAnalyserException {
+		CensusAnalyser censusAnalyser = new CensusAnalyser();
+		censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE, CSVBuilderType.OPEN_CSV);
+		String sortedCensusData = censusAnalyser.getStateWiseSortedCensusData();
+		CSVIndiaCensus[] censusList = new Gson().fromJson(sortedCensusData, CSVIndiaCensus[].class);
+		Assert.assertEquals(censusList[0].state, "Bihar");
 	}
 }
